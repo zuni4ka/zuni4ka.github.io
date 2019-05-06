11a84d67c1f72163873a1eb8cd238d1dea68234a
Maria Plieshka <zuni4ka@gmail.com>
2019-05-06 09:38:16 +0200

    Hello world

---
+++ precache-manifest.979a118c8b56814cce37fff8e76bb693.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a545c347361817081a67d28e65a2a11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f076e2d3c988ed1a27c3",
+    "url": "/static/css/main.3a5db715.chunk.css"
+  },
+  {
+    "revision": "8206ddf9d351031d494c",
+    "url": "/static/js/2.cf0d62a8.chunk.js"
+  },
+  {
+    "revision": "f076e2d3c988ed1a27c3",
+    "url": "/static/js/main.71d678c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

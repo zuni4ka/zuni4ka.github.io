2a068677548ad71602827d578f818ee5892124b2
Maria Plieshka <zuni4ka@gmail.com>
2019-05-06 09:34:17 +0200

    Prepare Hello world

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as styles from './App.module.css';
+
+function App() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Hello world!</h1>
+    </main>
+  );
+}
+
+export default App;

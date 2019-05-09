5db9760b5f37c4702212c1c1ee1fcabe641951db
Maria Plieshka <zuni4ka@gmail.com>
2019-05-09 12:57:54 +0200

    Add article and image

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ function App() {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Hello world!</h1>
+      <h2>My name is Maria.</h2>
+      <img src="../images/MyPhoto.jpg">My photo</img>
+      <article>
+        <p>Currently I am acvively immersing myself into the world of FrontEnd.</p>
+        <p>I am looking for a job opportunities in Berlin.</p>
+        <p>I would be happy to learn more, to work in a strong friendly team and bring some fresh ideas to it.</p>
+        <p>You can find out a bit more about me by visiting the additional pages of this site :)</p>
+      </article>  
     </main>
   );
 }

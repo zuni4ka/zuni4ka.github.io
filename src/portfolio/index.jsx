11e770f2177894204bf154db3545c0591db5f1c0
Maria Plieshka <zuni4ka@gmail.com>
2019-06-11 20:58:19 +0200

    Fill Portfolio page, add wrap property to footer

--- src/portfolio/index.jsx
+++ src/portfolio/index.jsx
@@ -3,12 +3,33 @@ import * as styles from './index.module.css';
 
 function Portfolio() {
   return (
-      <div className={styles.introduce}>
-        <p className={styles.description}>
-          There are my portfolio.
-        </p>
-      </div>
-  );
+    <div className={styles.introduce}>
+    <a
+      className={styles.certificate_link}
+      target="_blank"
+      href="https://zuni4ka.github.io/sedona/"
+      rel="noopener noreferrer"
+    >
+      <img
+        className={styles.certificate_img}
+        src={require('./project_1.jpg')}
+        alt="Professional HTML and CSS course project"
+      />
+    </a>
+    <a
+      className={styles.certificate_link}
+      target="_blank"
+      href="https://zuni4ka.github.io/candyshop/"
+      rel="noopener noreferrer"
+    >
+      <img
+        className={styles.certificate_img}
+        src={require('./project_2.jpg')}
+        alt="Professional JavaScript course project"
+      />
+    </a>
+  </div>
+);
 }
 
 export default Portfolio;

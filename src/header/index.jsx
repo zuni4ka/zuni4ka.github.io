4ad6a6e4fc7ed45fe5070e529c93d7c755f5b367
Maria Plieshka <zuni4ka@gmail.com>
2019-07-24 23:56:57 +0200

    Add logo link to homepage

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Content from '../content';
 import * as styles from './index.module.css';
 
@@ -6,9 +7,11 @@ const Header = (props) => {
     return (
         <header className={styles.header}>
             <Content className={styles.content}>
-                <svg viewBox="0 0 10 10" className={styles.z}>
-                    <path d="M1,1 L8,2 L2,8 L9,8" />
-                </svg>
+                <Link className={styles.logo} to="/">
+                    <svg viewBox="0 0 10 10" className={styles.z}>
+                        <path d="M1,1 L8,2 L2,8 L9,8" />
+                    </svg>
+                </Link>
                 {props.children}
             </Content>
         </header>
